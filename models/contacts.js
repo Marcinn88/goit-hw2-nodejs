6fe5819cc037177e1a54240c6f78577d3dc6543e
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-08-13 18:21:54 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -88,38 +88,10 @@ const updateContact = async (contactId, body) => {
   }
 };
 
-const validBody = (schema) => {
-  const func = (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-      next(
-        res.status(400).json({
-          message: "missing " + error.details[0].message + " field",
-        })
-      );
-    }
-    next();
-  };
-  return func;
-};
-
-const validEmptyBody = () => {
-  const func = (req, res, next) => {
-    const bodyEmpty = Object.keys(req.body).length === 0;
-    if (bodyEmpty) {
-      next(res.status(400).json({ message: "missing fields" }));
-    }
-    next();
-  };
-  return func;
-};
-
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-  // validBody,
-  // validEmptyBody
 }
