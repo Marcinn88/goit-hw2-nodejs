5a9b00bb5dfdf86f92590b0f899ce3aa2669acfa
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-09-09 11:23:27 +0200

    shema

--- models/contacts.model.js
+++ models/contacts.model.js
@@ -1,6 +1,27 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const users = new Schema ({
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter"
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+})
+
 const contact = new Schema({
     name: {
       type: String,
@@ -18,5 +39,6 @@ const contact = new Schema({
     },
   })
 const Contact = mongoose.model('contact', contact, "contacts")
+const Users = mongoose.model('users', users, "users")
 
-  module.exports = Contact;
+  module.exports = Contact, Users;
